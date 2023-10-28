fbb34a3497ef4989a3f66c016703f29d673e1358
lfinchHob <38416996+lfinchHob@users.noreply.github.com>
2023-10-28 19:50:43 +1100

    Create table.sql

---
+++ OTEL/python/threading/table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `words` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `hits` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1866177 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
